58872f7b8c1d8ad8831527c3fa88071d5f1cd7de
NomadArtisan <aeparenti@gmail.com>
2018-03-06 11:05:05 -0800

    changing over repositories commit

---
+++ Faucet Roguelike/Assets/src/Alex/AP_Count.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AP_Count {
+	public int minimum; 			//Minimum value for our Count class.
+	public int maximum; 			//Maximum value for our Count class.
+
+
+	//Assignment constructor.
+	public AP_Count (int min, int max)
+	{
+		minimum = min;
+		maximum = max;
+	}
+}
---
+++ Faucet Roguelike/Assets/src/Luke/LH_AutoRun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LH_AutoRun : MonoBehaviour 
+{
+	List<Vector2> pathThroughDungeon;
+	int nextIndex = 1 ; //index of next waypoint
+    private Vector2 normDir;
+    //normalized vector in the direction of the next waypoint
+	private float dist;
+	private float minDist = 0.25F;  
+    LH_Movement playerMovement;
+	
+	//List < Vector2 > yourList = myfunction();
+    void Start () {
+		pathThroughDungeon=FindObjectOfType<AP_DungeonGenerator>().GetPathThroughDungeon();
+		//gets list of Vector2's that guide the Player through level
+        playerMovement = FindObjectOfType<LH_Movement>();
+		//creates an instance of LH_Movment to guide Player through level
+	}
+	void Update () {
+	}
+
+	void FixedUpdate()
+	{
+		dist=Vector2.Distance(playerMovement.rigidPlayer.position,pathThroughDungeon[nextIndex]);
+		//Debug.Log("Distance: " + dist);
+		if(dist<minDist)
+		{
+            Debug.Log("Incrementing Index");
+			nextIndex++;
+			//return;
+		}
+	
+		normDir = (pathThroughDungeon[nextIndex]-playerMovement.rigidPlayer.position);
+        normDir.Normalize();
+        playerMovement.movePlayer(normDir);
+	}
+}
