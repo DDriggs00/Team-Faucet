88fbe448e34c20613878310864b1310adbe3dfcb
Miranda <35814733+geekforjesusmir@users.noreply.github.com>
2018-03-19 18:34:37 -0700

    healthbar work

--- Faucet Roguelike/Assets/src/Miranda/MR_Health.cs
+++ Faucet Roguelike/Assets/src/Miranda/MR_Health.cs
@@ -3,66 +3,36 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class MR_Health : MonoBehaviour {
+public class MR_Health : MonoBehaviour
+{
     LH_Health playerhealth;
-    public float CurrentHealth{ get; set; }
+    public float CurrentHealth { get; set; }
     public float MaxHealth { get; set; }
     public Slider healthbar;
-     
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start()
     {
         CurrentHealth = MaxHealth;
         playerhealth = FindObjectOfType<LH_Health>();
         healthbar.value = Calculatehealth();
-        int x = -1; 
-	}
+        int x = -1;
+    }
 
     float Calculatehealth()
     {
         return CurrentHealth / MaxHealth;
     }
-     
+
     // Update is called once per frame
-    void Update () {
-       
+    void Update()
+    {
+        healthbar.value = Calculatehealth();
 
     }
     void FixedUpdate()
     {
         playerhealth.getHP();
-        Debug.Log("health bar is displaying" + playerhealth.getHP());
-    }
-    public class PlayerHealth : MonoBehaviour
-    {
-        private int hp;
-
-        public int HP
-        {
-            get { return hp; }
-            set
-            {
-                if (value != hp)
-                {
-                    hp = value;
-                    Debug.Log("Player health set to: " + hp);
-                }
-            }
-        }
-    }
-
-    public class SomeClass : MonoBehaviour
-    {
-        public PlayerHealth playerHealth;
-
-        private int lastHP;
-
-        private void Update()
-        {
-            if (playerHealth.HP != lastHP)
-                Debug.Log("Player health has changed to: " + playerHealth.HP);
-
-            lastHP = playerHealth.HP;
-        }
+        
     }
 }
