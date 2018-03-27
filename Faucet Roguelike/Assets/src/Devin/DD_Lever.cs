52d70f9dfe4154b1c5644c39d9eba2c54da80652
DDriggs00 <DDriggs00@gmail.com>
2018-03-27 11:25:31 -0700

    Added lever code - Not 100% Working yet.

---
+++ Faucet Roguelike/Assets/src/Devin/DD_Lever.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DD_Lever : DD_Obstacle
+{
+
+	private bool mCanActivate = false;
+	private bool mLeverState = false; //true = on, false = off
+	DD_Lever()
+	{
+		Debug.Log("Lever Created");
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+
+		if(collision.tag == "Player")
+		{
+			// LH_Interact playerInteract = collision.gameObject.GetComponent<LH_Interact>();
+			// playerInteract.showInteraction(this);
+			mCanActivate = true;
+			Debug.Log("Player Lever Message Shown");
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if(collision.tag == "Player")
+		{
+			// LH_Health playerInteract = collision.gameObject.GetComponent<LH_Interact>();
+			// playerInteract.hideInteraction(this);
+			mCanActivate = false;
+			Debug.Log("Player Lever Message Hidden");
+		}
+	}
+
+	private void Interact() {
+
+	}
+	
+}
