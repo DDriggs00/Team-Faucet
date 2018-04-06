cafb7ae0ba1a0897823c79375602387d36a11b77
Luke Hill <34109335+littlemtn@users.noreply.github.com>
2018-04-06 12:18:33 -0700

    Implemented basic movement animation

--- Faucet Roguelike/Assets/src/Luke/LH_Sprite.cs
+++ Faucet Roguelike/Assets/src/Luke/LH_Sprite.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LH_Sprite : MonoBehaviour {
-	/* the player is going to be a circle for now */
+    /* the player is going to be a circle for now */
 
-	// Use this for initialization
+    // Use this for initialization
+
+    private Animator mAnimator;
+    private SpriteRenderer mSpriteRenderer;
 	void Start () {
-		
+		mAnimator = GetComponent<Animator>();
+        //mSpriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetAxis("Horizontal") < 0 )
+        {
+            mAnimator.SetTrigger("westKey");
+        }
+        else if (Input.GetAxis("Horizontal") > 0 )
+        {
+            mAnimator.SetTrigger("eastKey");
+        }
+        else if (Input.GetAxis("Vertical") < 0 )
+        {
+            mAnimator.SetTrigger("southKey");
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            mAnimator.SetTrigger("northKey");
+        }
+		else
+            mAnimator.SetTrigger("noKey");
+	}
+	void FixedUpdate()
+	{
+
 	}
 }
