c5241f2c74c2fbe682fc76b62ec47d63f4b45911
DDriggs00 <DDriggs00@gmail.com>
2018-04-16 22:05:33 -0700

    Added and Modified Various Code for Powerups

--- Faucet Roguelike/Assets/src/Devin/DD_Powerup.cs
+++ Faucet Roguelike/Assets/src/Devin/DD_Powerup.cs
@@ -6,6 +6,10 @@ public class DD_Powerup : DD_Obstacle
 {
 	private uint mHurtPotionValue = 10;
 	private int mHealPotionValue = 10;
+	private int mPositiveModifier = 200;
+	private int mNegativeModifier = 50;
+	private int mPowerupTime = 30;
+	private int mInvincibleTime = 15;
 
 	private ObstacleType mPowerupType;	//Amount of HP to heal
 	DD_Powerup()
@@ -36,21 +40,32 @@ public class DD_Powerup : DD_Obstacle
 		if(collision.tag == "Player")
 		{
 			LH_Health playerHP;
+			LH_Movement playerMovement;
 			switch (mPowerupType)
 			{
 				case ObstacleType.doubleArmor:
-					//Call DoubleArmor
+					playerHP = collision.gameObject.GetComponent<LH_Health>();
+					playerHP.changeArmor(mPositiveModifier);
 					break;
 				case ObstacleType.halfArmor:
-					//Call HalfArmor
+					playerHP = collision.gameObject.GetComponent<LH_Health>();
+					playerHP.changeArmor(mNegativeModifier);
 					break;
 				case ObstacleType.doubleDamage:
+					// playerHP = collision.gameObject.GetComponent<LH_Health>();
+					// playerHP.setD(mPositiveModifier);
 					break;
 				case ObstacleType.halfDamage:
+					// playerHP = collision.gameObject.GetComponent<LH_Health>();
+					// playerHP.changeArmor(mNegativeModifier);
 					break;
 				case ObstacleType.doubleSpeed:
+					playerMovement = collision.gameObject.GetComponent<LH_Movement>();
+					playerMovement.changeSpeed(mPositiveModifier);
 					break;
 				case ObstacleType.halfSpeed:
+					playerMovement = collision.gameObject.GetComponent<LH_Movement>();
+					playerMovement.changeSpeed(mNegativeModifier);
 					break;
 				case ObstacleType.healthPotion:
 					playerHP = collision.gameObject.GetComponent<LH_Health>();
