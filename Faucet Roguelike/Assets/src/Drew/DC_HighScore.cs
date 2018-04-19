520a8fa15f676184b46b23ec520d68becb819593
Thirsk <Nuraihyon@gmail.com>
2018-04-19 01:01:59 -0700

    Seed and high score
    Couldn't really test if the high score uploading was working properly as the server isn't up, but it should work.
    
    hssclient.exe is being included so it can easily be called from the Unity editor's play function.

---
+++ Faucet Roguelike/Assets/src/Drew/DC_HighScore.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class DC_HighScore : MonoBehaviour
+{
+	private string IP;
+	private int gameID;
+	
+	public DC_HighScore()
+	{
+		IP = "52.160.46.238";
+		gameID = 5;
+	}
+
+	public DC_HighScore(string overrideIP, int overrideGameID)
+	{
+		IP = overrideIP;
+		gameID = overrideGameID;
+	}
+	
+	public void UploadScore(string name, int score)
+	{
+		var highscore = new System.Diagnostics.Process();
+		highscore.StartInfo.FileName = "hssclient.exe";
+		highscore.StartInfo.Arguments = IP + " " + gameID + " " + name + " " + score;
+		highscore.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+		highscore.Start();
+	}
+}
---
+++ Faucet Roguelike/Assets/src/Drew/DC_Seed.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class DC_Seed : MonoBehaviour
+{   
+    private long seed;
+    
+    public void GenerateSeed()
+    {
+        seed = (SystemInfo.deviceUniqueIdentifier.GetHashCode() + System.DateTime.UtcNow.ToFileTimeUtc());
+        // Adds a hash of the HWID with the current system time as expressed as a long.
+    }
+    
+    public long GetSeed()
+    {
+        return seed;
+    }
+
+    public void SetSeed(long s)
+    {
+        seed = s;
+    }
+}
