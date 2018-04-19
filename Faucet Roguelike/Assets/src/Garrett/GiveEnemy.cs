18750e39f5cc4b5b39b36b5ca06f61ffa1b36da2
Goetken <oetkeng@gmail.com>
2018-04-19 00:10:59 -0700

    Enemy - GiveEnemy script
    script added to provide Minions and Bosses as enemis.

--- Faucet Roguelike/Assets/src/Garrett/GiveEnemy.cs
+++ Faucet Roguelike/Assets/src/Garrett/GiveEnemy.cs
@@ -1,29 +1,45 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//
-//public class GiveEnemy : MonoBehaviour {
-//
-//
-//	public Enemy mEnemy = new Enemy;
-//
-//	// Use this for initialization
-//	void Start () {
-//		
-//	}
-//	
-//	// Update is called once per frame
-//	void Update () {
-//		
-//	}
-//
-//
-//
-//	public generate()
-//	{
-//
-//
-//
-//
-//	}
-//}
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveEnemy : MonoBehaviour {
+
+	private int newEnemy = 0;
+	public Boss mBoss;
+	public Minion mMinion;
+
+	// Use this for initialization
+	void Start () {
+
+
+		for(int i=0;i<5;i++)
+		{
+			generate ();
+
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	public void generate()
+	{
+		
+		if(newEnemy==0)
+		{
+			Minion minionCopy = (Minion)Instantiate(mMinion,new Vector3(1 * 2.0F, 0, 0), Quaternion.identity);
+		}
+		else
+		{
+			//Boss bossCopy = (Boss)Instantiate(mBoss, new Vector3(1 * 2.0F, 0, 0), Quaternion.identity);
+
+		}
+
+
+	}
+}
