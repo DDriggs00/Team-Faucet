9f4540ef2d3abd3d5ca97a994b01145a1f4d89b1
zachmoreno90 <35829807+zachmoreno90@users.noreply.github.com>
2018-05-01 10:29:29 -0700

    Menu added before level generation

---
+++ Faucet Roguelike/Assets/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿// Zach Moreno
+// Unity Main Menu
+// Hunt for the Dodo Egg 2
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	
+	public void playGame()
+    {
+        Debug.Log("START!");
+        // Call Alex's function for random level generation, Level 1
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void loadGame()
+    {
+        // Call Drew's function to load a previosuly saved game
+        Debug.Log("LOAD!");
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("QUIT!");
+        // Universal call in Unity to exit from application
+        Application.Quit();
+    }
+}
